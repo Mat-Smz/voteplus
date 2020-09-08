e14ecdc03f9c8a37b8d5c095a6bc5241056d48ef
Mat-Smz <59562144+Mat-Smz@users.noreply.github.com>
2020-09-08 19:19:27 +0200

    clean code

--- Item.js
+++ Item.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 class Item extends Component {
   render() {
